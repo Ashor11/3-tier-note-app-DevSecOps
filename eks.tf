43daf4226320a4b2f20211e18d0b9d9807a0c69b
Ashour11 <ashouryasser11@gmail.com>
2025-05-13 07:00:26 +0300

    with aws

---
+++ eks.tf
@@ -0,0 +1,50 @@
+//create vpc with one subnet and create basic free instance in this subnet 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "eks" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "eks"
+  }
+}
+
+resource "aws_subnet" "eks" {
+  vpc_id                  = aws_vpc.eks.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true    //to make this subnet accessible from internet
+
+  tags = {
+    Name = "eks"
+  }         
+  }
+resource "aws_security_group" "eks" {  //create security group for this instance to allow all to all and all from all
+    vpc_id = aws_vpc.eks.id
+    
+    ingress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }   
+    }
+ 
+resource "aws_instance" "eks" {  //create instance in this subnet
+  ami           = "ami-0c55b159cbfafe1f0" //ubuntu 20.04
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.eks.id
+  vpc_security_group_ids = [aws_security_group.eks.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "eks"
+  }
+}
